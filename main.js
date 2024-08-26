09eb9e2c2bf71d5193c5c949d5d5d5d0c2d3bc4b
unknown <you@example.com>
2024-08-26 15:56:55 +0100

    first commit

--- main.js
+++ main.js
@@ -4,3 +4,19 @@ window.addEventListener('scroll', () =>{
     document.querySelector('nav').classList.toggle
     ('window-scroll', window.scrollY > 0)
 })
+
+
+// show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // change icon
+        const icon = faq.querySelector('.faq_icon i');
+        if(icon.className === '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgb(255, 255, 255);transform:msFilter"><path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"></path></svg>')
+       
+    })
+})
